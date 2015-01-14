ff66dcb6acb819c2def758968adf83f82a1d08c9
Parker Zink <parker.zink@gmail.com>
2015-01-13 17:41:14 -0800

    Added class and adjusted font size
    Added a new PHP function class and adjusted the font size from 135 to 100

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
             public $firstname;
             public $lastname;
             public $age;
+            public $mascot;
             
 
             public function __construct($firstname, $lastname, $age) {
@@ -25,6 +26,34 @@
           
       
         $me = new Human('Parker', 'Zink', 16);
+        
+        
+        echo $me->greet(); 
+        ?>
+<?php
+        
+        class Human2 {
+           
+            public $isAlive = true;
+            public $gender;
+            public $mascot;
+            public $school;
+            
+
+            public function __construct($gender, $mascot, $school) {
+              $this->gender = $gender;
+              $this->mascot = $mascot;
+              $this->school = $school;
+            }
+            
+           
+            public function greet() {
+              return " I am a " . $this->gender .  " and an official " . $this->mascot . " at " . $this->school . "!";
+            }
+          }
+          
+      
+        $me = new Human2('boy', 'bulldog', 'Pasadena High School');
         
         
         echo $me->greet(); 
